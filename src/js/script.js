788c806c25820a12be081104064ca709fed51732
DudaGod <dyda06@mail.ru>
2015-08-17 23:58:09 +0300

    new updates of mobile version

--- src/js/script.js
+++ src/js/script.js
@@ -7,17 +7,17 @@ var mob_menu_items = document.querySelectorAll(".page-nav__item");
 mob_menu_toggle_close.addEventListener("click", function(event) {
   event.preventDefault();
   for (var i = 0; i < mob_menu_items.length; i++) {
-    mob_menu_items[i].classList.toggle("page-nav__item--close");
+    mob_menu_items[i].classList.toggle("page-nav__item--open");
   }
-  mob_menu_toggle_close.classList.toggle("page-nav__item--close");
+  mob_menu_toggle_close.classList.toggle("page-nav__toggle-close--open");
 });
 
 
 mob_menu_toggle.addEventListener("click", function(event) {
   event.preventDefault();
   for (var i = 0; i < mob_menu_items.length; i++) {
-    mob_menu_items[i].classList.toggle("page-nav__item--close");
+    mob_menu_items[i].classList.toggle("page-nav__item--open");
   }
-  mob_menu_toggle_close.classList.toggle("page-nav__item--close");
+  mob_menu_toggle_close.classList.toggle("page-nav__toggle-close--open");
 });
 
