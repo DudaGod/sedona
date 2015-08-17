8dee2a0b7952a7f914980eeeb208cbbbd37edb23
DudaGod <dyda06@mail.ru>
2015-08-17 15:31:10 +0300

    mobile header is done

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var mob_menu_toggle = document.querySelector(".page-nav__toggle");
+var mob_menu_toggle_close = document.querySelector(".page-nav__toggle-close");
+var mob_menu_items = document.querySelectorAll(".page-nav__item");
+
+mob_menu_toggle_close.addEventListener("click", function(event) {
+  event.preventDefault();
+  for (var i = 0; i < mob_menu_items.length; i++) {
+    mob_menu_items[i].classList.toggle("page-nav__item--close");
+  }
+  mob_menu_toggle_close.classList.toggle("page-nav__item--close");
+});
+
+
+mob_menu_toggle.addEventListener("click", function(event) {
+  event.preventDefault();
+  for (var i = 0; i < mob_menu_items.length; i++) {
+    mob_menu_items[i].classList.toggle("page-nav__item--close");
+  }
+  mob_menu_toggle_close.classList.toggle("page-nav__item--close");
+});
+
