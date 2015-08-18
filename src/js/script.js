9f2792db6130deca75e3c43fcb82b56602cb25a6
DudaGod <dyda06@mail.ru>
2015-08-19 01:23:27 +0300

    mobile version is almost done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,44 @@
 "use strict";
 
+// JS OR NO JS
+
+var html = document.documentElement;
+html.className = html.className.replace("no-js", "js");
+
+
+// GOOGLE MAP
+
+function Initialize() {
+  var myLatLng = new google.maps.LatLng(34.7437805,-111.7485324);
+  var myPointLng = new google.maps.LatLng(34.868147,-111.7584029);
+  var contentString = 'Sedona';
+
+  var mapOptions = {
+    zoom: 9,
+    center: myLatLng
+  }
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myPointLng,
+    map: map
+  });
+
+  var infoWindow = new google.maps.InfoWindow ({
+    content: contentString
+  })
+
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infoWindow.open(map, marker);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', Initialize);
+
+
+// NAVIGATION MOBILE VRSION
+
 var mob_menu_toggle = document.querySelector(".page-nav__toggle");
 var mob_menu_toggle_close = document.querySelector(".page-nav__toggle-close");
 var mob_menu_items = document.querySelectorAll(".page-nav__item");
